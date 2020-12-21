95fe4002d2c8b67d75bd43da512e3a1a36924e8e
StefanoSalidu <st.salidu@gmail.com>
2020-12-21 10:00:24 +0100

    style: added table css for articles

--- mixins/HelpPageMixin.vue
+++ mixins/HelpPageMixin.vue
@@ -1,5 +1,5 @@
 <template>
-    <article>
+    <article class="help-section">
         <page-heading :title="article.title" :icon="['fas', 'question-circle']" />
 
         <help-breadcrumbs v-if="showBreadcrumbs" :data="articles" class="_margin-top-1" />
@@ -20,3 +20,74 @@ export default {
     }
 }
 </script>
+
+<style lang="scss">
+@import "~@inkline/inkline/src/css/mixins";
+@import "~@inkline/inkline/src/css/config";
+article.blog-post-page {
+        padding-right: $spacer-4;
+
+        img {
+            display:block;
+            max-width: 100%;
+            margin: $spacer-2 auto;
+            padding: 0 0 $spacer-1 $spacer-1;
+        }
+
+        table {
+            width: 100%;
+            margin: $spacer-2 0;
+            border-collapse: collapse;
+        }
+
+        tr:nth-of-type(even) {
+            background: lighten($color-primary, 60%);
+        }
+
+        th {
+            background: $color-primary;
+            color: $color-white;
+            font-weight: bold;
+        }
+
+        td, th {
+            padding: 6px;
+            border: 1px solid #e6e6e6;
+            text-align: left;
+        }
+
+        .nuxt-content-highlight {
+            margin: $spacer-1-2 0;
+        }
+
+        @include breakpoint-up(md) {
+            border-right: 1px solid #e6e6e6;
+        }
+
+        @include breakpoint-down(sm) {
+            padding: 0;
+            padding-bottom: $spacer-2;
+
+            h1 {
+                font-size: 1.5rem;
+                font-weight: bold;
+            }
+
+            h2 {
+                font-size: 1.25rem;
+                font-weight: bold;
+            }
+
+            img {
+                width: 100%;
+                padding: 0;
+            }
+
+            table {
+                table-layout: fixed;
+                width: 100%;
+                word-wrap: break-word;
+            }
+        }
+    }
+</style>
