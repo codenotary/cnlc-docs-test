be7ee5482acc01eab85e9e6df1f0aa1205ceed15
StefanoSalidu <st.salidu@gmail.com>
2020-12-21 12:23:57 +0100

    chore: enhanced VImg

--- components/global/VImg.vue
+++ components/global/VImg.vue
@@ -1,5 +1,5 @@
 <template>
-    <img :src="imgSrc()" :alt="alt">
+    <img :src="imgSrc()" :alt="alt" :style="getStyle">
 </template>
 
 <script>
@@ -12,6 +12,41 @@ export default {
         alt: {
             type: String,
             required: true
+        },
+        height: {
+            type: Number
+        },
+        minHeight: {
+            type: Number
+        },
+        maxHeight: {
+            type: Number
+        },
+        width: {
+            type: Number
+        },
+        minWidth: {
+            type: Number
+        },
+        maxWidth: {
+            type: Number
+        },
+        objectFit: {
+            type: String,
+            default: 'contain'
+        }
+    },
+    computed: {
+        getStyle () {
+            let style = '';
+            this.height && (style += `; height: ${this.height}`);
+            this.minHeight && (style += `; minHeight: ${this.minHeight}`);
+            this.maxHeight && (style += `; maxHeight: ${this.maxHeight}`);
+            this.width && (style += `; width: ${this.width}`);
+            this.minWidth && (style += `; minWidth: ${this.minWidth}`);
+            this.maxWidth && (style += `; maxWidth: ${this.maxWidth}`);
+            this.objectFit && (style += `; object-fit: ${this.objectFit}`);
+            return style;
         }
     },
     methods: {
